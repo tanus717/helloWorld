c610f82f044098328010b3ef691cb055cb31e466
Tarun Saini <Tarun Saini@InnovationM-115>
2019-10-03 16:05:04 +0530

    cmnd

--- demo/src/demo/map/Map.java
+++ demo/src/demo/map/Map.java
@@ -4,30 +4,21 @@ public class Map {
 
 	private int id;
 	private String name;
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+
+	public int getId() {
+		return id;
 	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Map other = (Map) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
